cbf090e6179a1e6b64a72786aea8624aa77d4e2e
paul errington <paul.errington@partnerize.com>
2019-10-15 19:53:53 +0100

    backup original config and edit in place.

--- web/add_cache_config.php
+++ web/add_cache_config.php
@@ -2,6 +2,8 @@
 
 include 'config.php';
 
+copy ( 'config.php', 'config.php'.date("c"));
+
 // Config for NON SSL, APC and MEMCACHE
 $CACHECFG = array ('csrf.disabled' => true,
   'memcache.local' => '\\OC\\Memcache\\APCu',
@@ -18,7 +20,7 @@ $NEWCONFIG = array_merge($CONFIG, $CACHECFG);
 
 $TOFILE = var_export($NEWCONFIG, TRUE);
 
-$fh = fopen('config_new.php', 'w') or die("can't open file");
+$fh = fopen('config.php', 'w') or die("can't open file");
 
 fwrite($fh,"<?php\n\$CONFIG = ".trim($TOFILE).";\n?>");
 
